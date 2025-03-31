37adc069542aa73674f17b8bff42971131938814
GountzJs <tomasjsdev@gmail.com>
2025-03-31 09:33:25 -0300

    feat(repositories): :sparkles: usersrepositories add quantitycards detail

--- src/repositories/users.repository.ts
+++ src/repositories/users.repository.ts
@@ -8,38 +8,44 @@ export class UsersRepository {
           u.id,
           u.login AS username,
           u.profile_image_url AS avatar,
-          COUNT(ub.id) AS quantityBorders,
-          RANK() OVER (ORDER BY COUNT(ub.id) DESC, u.login ASC) AS rank_position
+          COUNT(DISTINCT ub.id) AS quantityBorders,
+          COUNT(DISTINCT uc.id) AS quantityCards,
+          RANK() OVER (ORDER BY COUNT(DISTINCT ub.id) + COUNT(DISTINCT uc.id) DESC, u.login ASC) AS rank_position
         FROM users u
         LEFT JOIN user_borders ub ON u.id = ub.user_id
+        LEFT JOIN user_cards uc ON u.id = uc.user_id
         WHERE u.is_staff = false
         GROUP BY u.id
       ),
-
+  
       StaffUsers AS (
         SELECT
           u.id,
           u.login AS username,
           u.profile_image_url AS avatar,
-          COUNT(ub.id) AS quantityBorders,
+          COUNT(DISTINCT ub.id) AS quantityBorders,
+          COUNT(DISTINCT uc.id) AS quantityCards,
           NULL AS rank_position
         FROM users u
         LEFT JOIN user_borders ub ON u.id = ub.user_id
+        LEFT JOIN user_cards uc ON u.id = uc.user_id
         WHERE u.is_staff = true
         GROUP BY u.id
       )
-
+  
       SELECT
         id,
         username,
         avatar,
         quantityBorders,
+        quantityCards,
+        (quantityBorders + quantityCards) AS totalRank,
         CASE
           WHEN is_staff = true THEN 'CHALLENGER'
           WHEN rank_position = 1 THEN 'CHALLENGER'
           WHEN rank_position = 2 THEN 'MASTER'
           WHEN rank_position = 3 THEN 'DIAMOND'
-          WHEN rank_position = 4 THEN 'PLATINIUM'
+          WHEN rank_position = 4 THEN 'PLATINUM'
           WHEN rank_position = 5 THEN 'GOLD'
           WHEN rank_position = 6 THEN 'SILVER'
           WHEN rank_position = 7 THEN 'BRONZE'
